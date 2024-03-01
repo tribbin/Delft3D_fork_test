27fb59b88ecae2e107d6ee7be5f809606dcfac9a
Matthijs Huisman <matthijs.huisman@deltares.nl>
2024-03-01 14:56:22 +0100

    Couple of review comments
    Use fromisoformat instead of trying to parse the time ourselfs.
    Add comment explaining what could be done to improve the script
    format the python file using a formatter

--- test/deltares_testbench/minio-tools/remove_minio_versions.py
+++ test/deltares_testbench/minio-tools/remove_minio_versions.py
@@ -1,24 +1,30 @@
-import subprocess
 import json
-from datetime import datetime
-import pytz
+import subprocess
 from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
+
+import pytz
 
 # S3 Object Version Management Script
-# This Python script helps manage object versions in an Amazon S3 bucket. It 
-# uses the AWS CLI to list object versions, convert the output to JSON, and 
+# This Python script helps manage object versions in an Amazon S3 bucket. It
+# uses the AWS CLI to list object versions, convert the output to JSON, and
 # selectively delete versions based on their last modified timestamps.
 
 # Prerequisites
-# AWS CLI: Ensure that you have the AWS Command Line Interface (CLI) installed and configured with appropriate credentials to access your S3 bucket.
-# Usage
+# AWS CLI: Ensure that you have the AWS Command Line Interface (CLI) installed
+# and configured with appropriate credentials to access your S3 bucket.
+
+# Improvements
+# This script uses the AWS CLI as prerequisite. It was done this way because
+# this was a one of run. It would be better if it utelizes the minio python
+# library
 
 # Configuration:
 # Set the following parameters at the beginning of the script:
-bucket_name = 'dsc-testbench'  # bucket_name: The name of your S3 bucket.
-endpoint_url = 'https://s3.deltares.nl/'  # endpoint_url: The endpoint URL for your S3 bucket (e.g., https://s3.deltares.nl/).
-threshold_date_str = '2024-02-15'  # threshold_date_str: The threshold date (in the format ‘YYYY-MM-DD’) for version deletion.
-parallel_processes = 5  # parallel_processes: Number of parallel processes for deletion (adjust as needed).
+bucket_name = 'dsc-testbench'               # bucket_name: The name of your S3 bucket.
+endpoint_url = 'https://s3.deltares.nl/'    # endpoint_url: The endpoint URL for your S3 bucket (e.g., https://s3.deltares.nl/).
+threshold_date_str = '2024-02-15'           # threshold_date_str: The threshold date (in the format ‘YYYY-MM-DD’) for version deletion.
+parallel_processes = 5                      # parallel_processes: Number of parallel processes for deletion (adjust as needed).
 
 
 threshold_date_obj = datetime.strptime(threshold_date_str, '%Y-%m-%d').replace(tzinfo=pytz.UTC)
@@ -33,10 +39,7 @@ output_json = json.loads(output)
 
 def delete_object(version):
     last_modified_str = version['LastModified']
-    try:
-        last_modified_obj = datetime.strptime(last_modified_str, '%Y-%m-%dT%H:%M:%S.%f%z')
-    except ValueError:
-        last_modified_obj = datetime.strptime(last_modified_str, '%Y-%m-%dT%H:%M:%S%z')
+    last_modified_obj = datetime.fromisoformat(last_modified_str)
 
     if last_modified_obj > threshold_date_obj:
         delete_command = f'aws s3api delete-object --bucket {bucket_name} --key "{version['Key']}" --version-id {version['VersionId']} --endpoint-url {endpoint_url}'
