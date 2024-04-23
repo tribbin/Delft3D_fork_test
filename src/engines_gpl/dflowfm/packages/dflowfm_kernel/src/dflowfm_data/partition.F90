cd86fd7329b9e8547b2e6e41b26326b5e79fe409
Tijn Berends <tijn.berends@deltares.nl>
2024-04-23 13:14:44 +0200

    UNST-7786 bugfix for 'empty' structures
    In some cases, 'empty' structures (i.e. with zero geometry nodes in all partitions) exist in the model. Made sure that this doesn't crash any_structures_lie_across_multiple_partitions

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_data/partition.F90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_data/partition.F90
@@ -5366,10 +5366,9 @@ function any_structures_lie_across_multiple_partitions(nodeCountStru) result(res
       end if
       
       ! If more than one process lists >0 geometry nodes for a structure, then that structure lies across multiple partitions.
+      structure_lies_across_partitions = .false.
       if (n_partitions_with_nodes == 0) then
          call mess(LEVEL_WARN,'Programming error, please report: any_structures_lie_across_multiple_partitions found no partitions with nodes!')
-      elseif (n_partitions_with_nodes == 1) then
-         structure_lies_across_partitions = .false.
       elseif (n_partitions_with_nodes > 1) then
          structure_lies_across_partitions = .true.
       end if
