724c1c5984e12e24e63b8f6462410053b0568959
V <victor.chavarrias@deltares.nl>
2024-07-05 14:58:58 +0200

    removed `_mor` by pointing

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/compute_sediment/fm_mor_maxtimestep.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/compute_sediment/fm_mor_maxtimestep.f90
@@ -31,12 +31,14 @@
 ! 
 
    subroutine fm_mor_maxtimestep()
-   use m_flowtimes
-   use m_flow
-   use m_flowgeom
-   use m_sediment
-   use m_partitioninfo
-   use m_fm_erosed, only: sxtot, sytot, cdryb, morfac, lsedtot, ndx_mor, nd_mor, ln_mor
+   use m_flowtimes, only: dts
+   use m_flow, only: eps10, jamapflowanalysis, kkcflmx, limitingTimestepEstimation
+   use m_flowgeom, only: acl, ba, csu, snu, wu
+   use m_sediment, only: dzbdtmax, kcsmor
+   use m_fm_erosed, only: sxtot, sytot, cdryb, morfac, lsedtot
+   use m_fm_erosed, only: ndx=>ndx_mor
+   use m_fm_erosed, only: nd=>nd_mor
+   use m_fm_erosed, only: ln=>ln_mor
 
    implicit none
 
@@ -47,19 +49,16 @@
    kkcflmxloc = 0
    mf = max(morfac,1d0)
 
-   do k = 1, ndx_mor
+   do k = 1, ndx
       if (kcsmor(k)==0) then
          cycle
       endif
       !
       dum = 0.d0
-      do kk = 1, nd_mor(k)%lnx
-         !L = iabs(nd(k)%ln(kk))
-         !k1 = ln(1,L)
-         !k2 = ln(2,L)
-            L = iabs(nd_mor(k)%ln(kk))
-            k1 = ln_mor(1,L)
-            k2 = ln_mor(2,L)
+      do kk = 1, nd(k)%lnx
+            L = iabs(nd(k)%ln(kk))
+            k1 = ln(1,L)
+            k2 = ln(2,L)
             ac1 = acl(L)
             ac2 = 1d0-ac1
          do ised = 1, lsedtot
