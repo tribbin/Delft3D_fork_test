bb307a413d11ba359d1b6e9530def0b0fc856377
jjnoort <jan.noort@deltares.nl>
2024-07-23 13:26:48 +0200

    Update fm_external_forcings.f90

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/timespace/fm_external_forcings.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/timespace/fm_external_forcings.f90
@@ -2675,14 +2675,11 @@ contains
       use m_wind, only: patm
       use m_cell_geometry, only: ndx
       use m_alloc, only: aerr
-      use precision_basics, only: hp
-
-      real(kind=hp), intent(in) :: default_value !< default atmospheric pressure value
       integer :: status
 
       status = 0
       if (.not. allocated(patm)) then
-         allocate (patm(ndx), stat=status, source=default_value)
+         allocate (patm(ndx), stat=status, source=0.d0)
          call aerr('patm(ndx)', status, ndx)
       end if
 
