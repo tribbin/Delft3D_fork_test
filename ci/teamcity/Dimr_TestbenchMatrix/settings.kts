82b9f6e69e4417d95313a9e9b7330d194d55525d
Robin van Westrenen <robin.vanwestrenen@deltares.nl>
2024-09-09 09:35:01 +0000

    Add new build configs to settings.kts

--- ci/teamcity/Dimr_TestbenchMatrix/settings.kts
+++ ci/teamcity/Dimr_TestbenchMatrix/settings.kts
@@ -10,15 +10,21 @@ import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import testbenchMatrix.Trigger
 import testbenchMatrix.Linux
 import testbenchMatrix.Windows
+import testbenchMatrix.LinuxApprove
+import testbenchMatrix.WindowsApprove
+import testbenchMatrix.Release
 
 version = "2024.03"
 
 project {
-    description = "contact: BlackOps"
+    description = "contact: BlackOps (black-ops@deltares.nl)"
 
     buildType(Trigger)
-    buildType(Windows)
     buildType(Linux)
+    buildType(Windows)
+    buildType(LinuxApprove)
+    buildType(WindowsApprove)
+    buildType(Release)
 
-    buildTypesOrder = arrayListOf(Trigger, Linux, Windows)
+    buildTypesOrder = arrayListOf(Trigger, Linux, Windows, LinuxApprove, WindowsApprove, Release)
 }
