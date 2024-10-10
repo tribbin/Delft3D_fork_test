20a5d4fd172ac5c487c83211573aec8350ca6e15
reyns <Johan.Reyns@deltares.nl>
2024-10-10 15:41:50 +0200

    UNST-8366
    Review comments

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/compute/furu.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/compute/furu.f90
@@ -165,7 +165,7 @@
                 if (modind < 9) then
                    frL = cfwavhi(L) * hypot(u1L - ustokes(L), v(L) - vstokes(L))
                 elseif (modind == 9) then
-                   frL = cfhi_vanrijn(L) * hypot(u1L - ustokes(L), v(L) - vstokes(L)) ! g/ca**2/h/rho
+                   frL = cfhi_vanrijn(L) * hypot(u1L - ustokes(L), v(L) - vstokes(L))
                 elseif (modind == 10) then ! Ruessink 2003
                    uorbL = .5d0 * (uorb(k1) + uorb(k2))
                    frL = cfuhi(L) * sqrt((u1L - ustokes(L))**2 + (v(L) - vstokes(L))**2 + (1.16d0 * uorbL * fsqrtt)**2)
