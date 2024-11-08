9944e158de67472cbddd4281b05e283fec3afd4a
Jeroen Noort <jeroen.noort@deltares.nl>
2024-11-08 11:02:28 +0100

    fix syntax error

--- ci/teamcity/scripts/check_scope.py
+++ ci/teamcity/scripts/check_scope.py
@@ -91,6 +91,6 @@ if __name__ == "__main__":
     component = indexer.get_scope("ci/teamcity/Dimr_TestbenchMatrix/repo_index.json", changes)
 
     print(
-        f"Determined scope: {component}\n----------- CHANGES -----------\n{"\n".join(indexer.changes_with_component)}"
+        f"Determined scope: {component}\n----------- CHANGES -----------\n{'\n'.join(indexer.changes_with_component)}"
     )
     print(f"##teamcity[setParameter name='branch_name' value='{component}']")
