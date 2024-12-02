79ecfde9010bbb4b459d7e0d34962023f9731834
huisman <matthijs.huisman@deltares.nl>
2024-12-02 11:00:21 +0100

    Use cmd instead of entrypoint

--- ci/dockerfiles/set-env.cmd
+++ ci/dockerfiles/set-env.cmd
@@ -1,1 +1,5 @@
-"C:\\Program Files (x86)\\Intel\\oneAPI\\setvars.bat" && "C:\\Program Files (x86)\\Intel\\oneAPI\\setvars-vcvarsall.bat" && "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\Common7\\Tools\\VsDevCmd.bat"
+@echo off
+call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat"
+call "C:\Program Files (x86)\Intel\oneAPI\setvars-vcvarsall.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
+cmd /K
