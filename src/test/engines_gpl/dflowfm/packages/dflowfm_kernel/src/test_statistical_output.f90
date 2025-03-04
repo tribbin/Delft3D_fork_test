72f85ad81f096e953da3ab61bf63fed7a0725f48
Harmen Wierenga <harmen.wierenga@deltares.nl>
2025-03-04 16:30:14 +0100

    Add tests for more functions in statistical output

--- src/test/engines_gpl/dflowfm/packages/dflowfm_kernel/src/test_statistical_output.f90
+++ src/test/engines_gpl/dflowfm/packages/dflowfm_kernel/src/test_statistical_output.f90
@@ -23,6 +23,7 @@
 
 module test_statistical_output
    use m_statistical_output
+   use m_statistical_output_types, only: SO_CURRENT, SO_AVERAGE
    use ftnunit
    use precision, only: dp
    implicit none
@@ -33,13 +34,40 @@ module test_statistical_output
    public :: tests_statistical_output
 
 contains
+
    subroutine tests_statistical_output
       call test(test_realloc_unallocated, 'Tests realloc function with unallocated array')
       call test(test_realloc_allocated, 'Tests realloc function with allocated array')
       call test(test_realloc_crop_no_elements, 'Tests realloc function with crop and no elements')
       call test(test_realloc_crop_one_element, 'Tests realloc function with crop and an element')
       call test(test_dealloc, 'Tests dealloc function')
+      call test(test_update_source_input, 'Tests update_source_input function')
+      call test(test_update_statistical_output_average, 'Tests update_statistical_output function with average operation')
    end subroutine tests_statistical_output
+
+   subroutine process_data_doubler(data_pointer)
+      use precision, only: dp
+      real(kind=dp), pointer, dimension(:), intent(inout) :: data_pointer
+      data_pointer = data_pointer * 2.0_dp
+   end subroutine process_data_doubler
+
+   function create_output_item(source_input, operation_type) result(output_item)
+      real(kind=dp), target, dimension(:), intent(inout) :: source_input
+      integer, intent(in) :: operation_type
+      type(t_output_variable_item) :: output_item
+      output_item%id_var = 42
+      output_item%operation_type = operation_type
+      output_item%source_input => source_input
+      call initialize_statistical_output(output_item)
+   end function create_output_item
+
+   function create_simple_output_variable_set(source_input) result(output_set)
+      real(kind=dp), target, dimension(:), intent(inout) :: source_input
+      type(t_output_variable_set) :: output_set
+      output_set%count = 1
+      call realloc(output_set)
+      output_set%statout(1) = create_output_item(source_input, SO_CURRENT)
+   end function create_simple_output_variable_set
 
    subroutine test_realloc_unallocated()
       type(t_output_variable_set) :: output_set
@@ -89,4 +117,29 @@ contains
       call dealloc(output_set)
       call assert_equal(allocated(output_set%statout), .false., '')
    end subroutine test_dealloc
+
+   subroutine test_update_source_input()
+      type(t_output_variable_set) :: output_set
+      real(kind=dp), dimension(3), target :: source_input
+      source_input = [1.0_dp, 2.0_dp, 3.0_dp]
+      output_set = create_simple_output_variable_set(source_input)
+      output_set%statout(1)%source_input_function_pointer => process_data_doubler
+      call update_source_input(output_set)
+      call assert_comparable(output_set%statout(1)%source_input, [2.0_dp, 4.0_dp, 6.0_dp], 1e-5_dp, '')
+   end subroutine test_update_source_input
+
+   subroutine test_update_statistical_output_average()
+      type(t_output_variable_item) :: output_item
+      real(kind=dp), dimension(3), target :: source_input
+      real(kind=dp) :: time_step
+      source_input = [-1.0_dp, 2.0_dp, -3.0_dp]
+      output_item = create_output_item(source_input, SO_AVERAGE)
+      time_step = 2.0_dp
+      call update_statistical_output(output_item, time_step)
+      source_input = [2.0_dp, 1.0_dp, 0.0_dp]
+      call update_statistical_output(output_item, time_step)
+
+      call assert_comparable(output_item%stat_output, [2.0_dp, 6.0_dp, -6.0_dp], 1e-5_dp, '')
+      call assert_comparable(output_item%time_step_sum, 4.0_dp, 1e-5_dp, '')
+   end subroutine test_update_statistical_output_average
 end module test_statistical_output
