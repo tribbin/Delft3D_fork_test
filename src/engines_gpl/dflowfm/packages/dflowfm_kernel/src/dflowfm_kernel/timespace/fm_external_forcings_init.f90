aeb9bdc95cb2f3959769d7cf809110555313e23a
Erwin Mulder <erwin.mulder@deltares.nl>
2025-08-12 10:25:32 +0200

    Fixed formatting specifier in error messages.

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/timespace/fm_external_forcings_init.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/timespace/fm_external_forcings_init.f90
@@ -920,7 +920,7 @@ contains
             btempforcingtypD = .true.
          case ('solarradiation')
             if (net_solar_radiation_available) then
-                write (msgbuf, '(7a)') 'quantity = ', trim(quantity), ' cannot be combined with netsolarradiation.'
+                write (msgbuf, '(3a)') 'quantity = ', trim(quantity), ' cannot be combined with netsolarradiation.'
                 call err_flush()
                 return
             end if
@@ -928,7 +928,7 @@ contains
             solar_radiation_available = .true.
          case ('netsolarradiation')
             if (solar_radiation_available) then
-                write (msgbuf, '(7a)') 'quantity = ', trim(quantity), ' cannot be combined with solarradiation.'
+                write (msgbuf, '(3a)') 'quantity = ', trim(quantity), ' cannot be combined with solarradiation.'
                 call err_flush()
                 return
             end if
