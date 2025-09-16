789ad687ae23617e9be90716616c7a12d7a7c9a2
Harmen Wierenga <harmen.wierenga@deltares.nl>
2025-09-16 10:52:50 +0200

    Change to pre-build step, since it should fail as quickly as possible

--- src/cmake/compiler_options/automatic_conversions.cmake
+++ src/cmake/compiler_options/automatic_conversions.cmake
@@ -4,7 +4,7 @@ function(check_style_double_precision target_name)
     endif()
     get_target_property(target_source_dir ${target_name} SOURCE_DIR)
     add_custom_command(TARGET ${target_name}
-                       POST_BUILD
+                       PRE_BUILD
                        COMMAND Python::Interpreter convert_double.py "--check" "--directory" "${target_source_dir}"
                        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/../../tools/double_precision_conversion
                        VERBATIM
