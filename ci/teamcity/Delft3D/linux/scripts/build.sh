98d37a74495f8405a4cebe5d358b5a3ed36f94e4
hrajagers <hrajagers@gmail.com>
2025-09-16 15:28:48 +0200

    Need to set MSAN_SYMBOLIZER_PATH ...

--- ci/teamcity/Delft3D/linux/scripts/build.sh
+++ ci/teamcity/Delft3D/linux/scripts/build.sh
@@ -23,5 +23,12 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
+echo Searching LLVM ...
+find / -iname "llvm-symbolizer"
+
+echo Defining MSAN_SYMBOLIZER_PATH ...
+export MSAN_SYMBOLIZER_PATH=/opt/intel/oneapi/compiler/2024.2/bin/compiler/llvm-symbolizer
+
+echo Building ...
 cmake -S ./src/cmake -G "${GENERATOR}" -D CONFIGURATION_TYPE:STRING="${PRODUCT}" -D CMAKE_BUILD_TYPE="${BUILD_TYPE}" -B "build_${PRODUCT}" -D CMAKE_INSTALL_PREFIX="build_${PRODUCT}/install"
 cmake --build "build_${PRODUCT}" --parallel --config "${BUILD_TYPE}"
