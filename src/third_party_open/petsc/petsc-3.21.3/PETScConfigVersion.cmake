a42835cc2425abe3eb90624f441f0d83a08888bc
Harmen Wierenga <harmen.wierenga@deltares.nl>
2026-01-07 10:49:37 +0100

    Prepare PETSc to be used as a dependency by preCICE later.
    Copy over petscadvancedmacros.h from the relevant version of PETSc, which we had removed for efficiency.
    Generate PETScConfig.cmake using LLM

---
+++ src/third_party_open/petsc/petsc-3.21.3/PETScConfigVersion.cmake
@@ -0,0 +1,20 @@
+# PETSc CMake Version File
+
+set(PACKAGE_VERSION "3.21.3")
+
+# Check whether the requested PACKAGE_FIND_VERSION is compatible
+if("${PACKAGE_VERSION}" VERSION_LESS "${PACKAGE_FIND_VERSION}")
+  set(PACKAGE_VERSION_COMPATIBLE FALSE)
+else()
+  set(PACKAGE_VERSION_COMPATIBLE TRUE)
+  if("${PACKAGE_VERSION}" VERSION_EQUAL "${PACKAGE_FIND_VERSION}")
+    set(PACKAGE_VERSION_EXACT TRUE)
+  endif()
+endif()
+
+# Check whether the requested PACKAGE_FIND_VERSION is compatible with major version
+if("${PACKAGE_FIND_VERSION_MAJOR}" STREQUAL "3")
+  set(PACKAGE_VERSION_COMPATIBLE TRUE)
+else()
+  set(PACKAGE_VERSION_COMPATIBLE FALSE)
+endif()
